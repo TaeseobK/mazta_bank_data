bc1a739fbaf0186ee8c75efad787a0fd60ad57e9
bayua <bayuafrizal830@gmail.com>
2025-03-07 16:23:06 +0700

    aaa

--- deploy.sh
+++ deploy.sh
@@ -20,8 +20,9 @@ pipenv run python master_data/manage.py makemigrations
 
 # Run migrations
 echo "Running migrations..."
-pipenv run python master_data/manage.py migrate
 pipenv run python master_data/manage.py migrate master --database=master
+pipenv run python master_data/manage.py migrate sales --database=sales
+pipenv run python master_data/manage.py migrate
 
 # Collect static files
 echo "Collecting static files..."
