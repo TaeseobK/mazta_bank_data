b56ff900e4e0992b5e5ab7ad1b2aea88339c8018
bayua <bayuafrizal830@gmail.com>
2025-03-07 16:23:50 +0700

    aaa

--- deploy.ps1
+++ deploy.ps1
@@ -11,8 +11,9 @@ Write-Host "Running internal migrations..."
 pipenv run py master_data/manage.py makemigrations
 
 Write-Host "Running database migrations..."
-pipenv run py master_data/manage.py migrate
 pipenv run py master_data/manage.py migrate master --database=master
+pipenv run py master_data/manage.py migrate sales --database=sales
+pipenv run py master_data/manage.py migrate
 
 Write-Host "Collect static files..."
 pipenv run py master_data/manage.py collectstatic
