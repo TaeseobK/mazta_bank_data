1e8a47dfb07140333921632c55dfe92171806158
satrio <bayuafrizal830@gmail.com>
2025-09-04 10:06:49 +0700

    penambahan admin untuk supplier

--- master_data/supplier/admin.py
+++ master_data/supplier/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class VendorAdmin(admin.ModelAdmin) :
+    list_display = [field.name for field in Vendor._meta.get_fields() if field.name not in ['id', 'created_at']]
+    list_filter = ('name', 'entity',)
+    list_display_links = [field.name for field in Vendor._meta.get_fields() if field.name not in ['id', 'created_at']]
+    search_fields = [field.name for field in Vendor._meta.get_fields() if field.get_internal_type() == 'CharField']
+
+class PrincipleAdmin(admin.ModelAdmin) :
+    list_display = [field.name for field in Principle._meta.get_fields() if field.name not in ['id', 'created_at']]
+    list_filter = ('details',)
+    list_display_links = [field.name for field in Principle._meta.get_fields() if field.name not in ['id', 'created_at']]
+    search_fields = [field.name for field in Principle._meta.get_fields() if field.get_internal_type() == 'CharField']
+
+admin.site.register(Vendor, VendorAdmin)
+admin.site.register(Principle, PrincipleAdmin)
